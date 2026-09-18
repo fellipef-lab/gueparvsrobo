a7518241f21aef9cb8f600094d4c7d89ef46d963
fellipef-lab <fellipe.f@grupodime.com.br>
2026-09-18 16:19:06 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,8 @@ import { iniciarNav, ativarAba } from './nav.js';
 import { carregarTudo } from './dados.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  // Inicializa o sistema e escuta a sessão
   await iniciar();
-  
-  // Configura os cliques do menu
   iniciarNav();
-  
-  // Força a exibição e o carregamento do Dashboard por padrão
   ativarAba('dashboard');
   carregarTudo();
 });
