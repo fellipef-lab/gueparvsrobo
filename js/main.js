97cd485ec2a814b1af2e1eecb98253a57220066b
fellipef-lab <fellipe.f@grupodime.com.br>
2026-09-18 16:24:47 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ import { iniciarNav, ativarAba } from './nav.js';
 import { carregarTudo } from './dados.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
+  // Inicializa autenticação e estado do Supabase
   await iniciar();
+
+  // Ativa os cliques nos botões do menu lateral
   iniciarNav();
+
+  // Define Dashboard como inicial e carrega o banco
   ativarAba('dashboard');
   carregarTudo();
 });
